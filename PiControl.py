4deb3c643b2df09728a4d9a8e88b71c465567d5c
Lance Bongon <lbbongon@gmail.com>
2019-05-31 15:33:56 +1000

    Fixed the issue with login and finished the function of the garage door.

--- PiControl.py
+++ PiControl.py
@@ -1,10 +1,17 @@
 import logging
 import psycopg2
+import time
+from database import Database_FetchUser, Database_FetchPassword
 from flask import Flask, render_template, request, flash, redirect, url_for
 from config import Config
 from forms import LoginForm
-
+import RPi.GPIO as GPIO
 app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21, GPIO.OUT)
+
 app.config.from_object(Config)
 
 webpage = {'homecontrol': 'Welcome to Home Control', 'garagecontrol': 'Welcome to Garage Control'}
@@ -13,19 +20,36 @@ webpage = {'homecontrol': 'Welcome to Home Control', 'garagecontrol': 'Welcome t
 def index():
     form = LoginForm()
     if request.method == 'POST' and form.validate_on_submit():
+        
         familyData_username = form.username.data
         familyData_password = form.password.data
         
-        return redirect(url_for('GaragePage'))
+        fetch_html_username = Database_FetchUser(familyData_username)
+        fetch_html_password = Database_FetchPassword(familyData_password)
+        app.logger.info(fetch_html_username)
+        app.logger.info(fetch_html_password)
+        
+        if familyData_username == fetch_html_username and fetch_html_password == True:
+            return redirect(url_for('GaragePage'))
+        else:
+            app.logger.info("Try Again")
+        
     return render_template('HomeControl.html', title = webpage, form=form)
 
 
-
-        
 @app.route('/GaragePage', methods=['GET', 'POST'])
 def GaragePage():
-     
-     return render_template('GarageDoor.html')
+    return render_template('GarageDoor.html')
+    
+@app.route('/<action>')
+def GarageDoor(action):
+    if action == "on":
+        GPIO.output(21, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(21, GPIO.LOW)
+    else:
+        GPIO.output(21, GPIO.LOW)
+    return render_template('GarageDoor.html')
     
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,10 +1,69 @@
 import psycopg2
+import logging
+con = None
+results = None
+testData = None
 
-def Database_Query():
-    conn = psycopg2.connect('dbname=familyaccess')
-    pointer = conn.cursor()
-    pointer.execute('select * from family')
-    results = cur.fetchall()
 
-for result in results:
-    print(result)
+def Database_Version():
+    try:
+        conn = psycopg2.connect(host="127.0.0.1", database="familyaccess", user="postgres", password="password", port="5432")
+        cur = conn.cursor()
+        print('PostgreSQL database version:')
+        cur.execute("Select version()")
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+            
+#Fetch and returns the username gathered from postgres database.           
+def Database_FetchUser(username):
+    familyName = None
+    try:
+        conn = psycopg2.connect(host="127.0.0.1", database="familyaccess", user="postgres", password="password", port="5432")
+        cur = conn.cursor()
+        print('PostgreSQL database version:')
+        cur.execute("SELECT * FROM familymembers WHERE family_user = %s",(username,))
+        # display the PostgreSQL database server version
+        db_fetch_username = cur.fetchone()
+        familyName = db_fetch_username[1]
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+    return familyName
+#Fetch and returns the username gathered from postgres database.
+def Database_FetchPassword(password):
+    familyPassword = None
+    try:
+        conn = psycopg2.connect(host="127.0.0.1", database="familyaccess", user="postgres", password="password", port="5432")
+        cur = conn.cursor()
+        print('PostgreSQL database version:')
+        cur.execute("SELECT * FROM familymembers WHERE family_password = crypt(%s, family_password)",(password,))
+        # display the PostgreSQL database server version
+        db_fetch_password = cur.fetchone()
+        if cur.rowcount == 1:
+            familyPassword = True
+        elif cur.rowcount == 0:
+            familyPassword = False
+        else:
+            familyPassword = False
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+    return familyPassword
+
+
